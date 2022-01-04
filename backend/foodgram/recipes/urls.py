23020d8d1972ae7d7b7207d201c2c951f9da9aa7
Robin <barabinr@list.ru>
2022-01-05 00:31:22 +0300

    debuging after review

--- backend/foodgram/recipes/urls.py
+++ backend/foodgram/recipes/urls.py
@@ -1,13 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (
-    IngredientViewSet,
-    RecipeViewSet,
-    ShoppingCartView,
-    TagViewSet,
-    download_shopping_cart,
-)
+from .views import (IngredientViewSet, RecipeViewSet, ShoppingCartView,
+                    TagViewSet, download_shopping_cart)
 
 router = DefaultRouter()
 router.register('tags', TagViewSet, basename='tags')
--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -6,26 +6,16 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from users.serializers import RecipeSubSerializer
 
 from .filters import RecipeFilter
-from .models import (
-    Favorite,
-    Ingredient,
-    Recipe,
-    RecipeIngredient,
-    ShoppingCart,
-    Tag,
-)
+from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                     ShoppingCart, Tag)
 from .permissions import IsRecipeOwnerOrReadOnly
-from .serializers import (
-    FavoriteSerializer,
-    IngredientSerialiser,
-    RecipeReadSerializer,
-    RecipeWriteSerializer,
-    ShoppingCartSerializer,
-    TagSerializer,
-)
+from .serializers import (FavoriteSerializer, IngredientSerialiser,
+                          RecipeReadSerializer, RecipeWriteSerializer,
+                          ShoppingCartSerializer, TagSerializer)
 
 
 class TagViewSet(viewsets.ModelViewSet):
